e82a33c754c3dd67534982bfd780c2050c42d9d4
J0J0XMR <163790330+J0J0XMR@users.noreply.github.com>
2024-10-22 11:40:46 -0600

    RangeError Fix
    See: https://github.com/mrtnetwork/bitcoin_base/issues/9

--- lib/src/bitcoin/script/script.dart
+++ lib/src/bitcoin/script/script.dart
@@ -47,7 +47,7 @@ class Script {
         commands.add(BytesUtils.toHexString(scriptBytes.sublist(index, index + bytesToRead)));
         index = index + bytesToRead;
       } else {
-        final viAndSize = IntUtils.decodeVarint(scriptBytes.sublist(index, index + 9));
+        final viAndSize = IntUtils.decodeVarint(scriptBytes.sublist(index));
         int dataSize = viAndSize.item1;
         int size = viAndSize.item2;
         final lastIndex = (index + size + dataSize) > scriptBytes.length
